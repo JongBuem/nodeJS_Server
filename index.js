560a7a3cfb965d7111246af56c9d1aee32da5083
JongBuem <whdqja1918@naver.com>
2021-12-04 20:57:07 +0900

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+const http = require('http');
 
-
-app.get('/',(req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain'}); 
-    res.end('My JB\n');
-})
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+});
 
 const port = process.env.PORT || 1337;
-app.listen(port);
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
