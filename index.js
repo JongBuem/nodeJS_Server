9ae792c6700e118f9a5f2044b8d46734f34e4b35
JongBuem <whdqja1918@naver.com>
2021-12-04 18:46:39 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/',(req, res)=>{
+    res.writeHead(200, {'Content-Type': 'text/plain'}); 
+    res.end('My JB\n');
+})
+
+app.listen(8080)
